b1041d2c2af3391e7b1ad29ec68df62f83502880
chougar youva <chougar.abdelhadi@gmail.com>
2023-09-18 13:44:17 +0200

    Creer l'entité Etudiant avant ces champs et creer la table dans le server MySQL

--- src/main/java/fr/vde/tpspring/entities/Etudiant.java
+++ src/main/java/fr/vde/tpspring/entities/Etudiant.java
@@ -1,4 +1,29 @@
 package fr.vde.tpspring.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "ETUDIANT")
+@Data @NoArgsConstructor @AllArgsConstructor
 public class Etudiant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String nom;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private int tel;
+
+    private String adresse;
+
+
 }
