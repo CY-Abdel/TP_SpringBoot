c0b2089944a858d963d2a20416be9646f554d46f
chougar youva <chougar.abdelhadi@gmail.com>
2023-09-18 13:05:30 +0200

    First commit : Ajouter mon projet

---
+++ src/test/java/fr/vde/tpspring/TpSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.vde.tpspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
