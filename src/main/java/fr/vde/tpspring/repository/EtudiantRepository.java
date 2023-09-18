6e36de9500a3437fce2380937ef88be595bee11c
chougar youva <chougar.abdelhadi@gmail.com>
2023-09-18 13:46:20 +0200

    Creer le Repository EtudiantRepository et extends JpaRepository

--- src/main/java/fr/vde/tpspring/repository/EtudiantRepository.java
+++ src/main/java/fr/vde/tpspring/repository/EtudiantRepository.java
@@ -1,4 +1,10 @@
 package fr.vde.tpspring.repository;
 
-public interface EtudiantRepository {
+import fr.vde.tpspring.entities.Etudiant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EtudiantRepository extends JpaRepository<Etudiant, Integer> {
+
 }
