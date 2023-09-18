c4840bdc57490f0294ce28f4358b99d935a96993
chougar youva <chougar.abdelhadi@gmail.com>
2023-09-18 13:50:20 +0200

    Creer le service et creer l'entiteRepository et son constructeur avec paramètre

--- src/main/java/fr/vde/tpspring/service/EtudiantService.java
+++ src/main/java/fr/vde/tpspring/service/EtudiantService.java
@@ -1,4 +1,16 @@
 package fr.vde.tpspring.service;
 
+import fr.vde.tpspring.repository.EtudiantRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EtudiantService {
+    private EtudiantRepository etudiantRepository;
+
+    //    constructeur
+    public EtudiantService(EtudiantRepository etudiantRepository) {
+        this.etudiantRepository = etudiantRepository;
+    }
+
+    
 }
